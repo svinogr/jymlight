4db2c1ea4fe831aba6e58344ff535738fd75b4fb
32947 <3294747@mail.ru>
2024-01-05 12:23:16 +0300

    Initial commit 05.01.24

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "jymlight"
+include(":app")
+include(":database")
